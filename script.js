9a88a158b720f5d012f8141294a3d6e6dc9ac43c
Jayavenkate <jayajaikithuja@gmail.com>
2023-01-04 12:47:56 +0300

    call back task

---
+++ script.js
@@ -0,0 +1,44 @@
+setTimeout(()=>{
+    console.log(10);
+    
+    setTimeout(()=>{
+    console.log(9);
+   
+    setTimeout(()=>{
+        console.log(8);
+        
+        setTimeout(()=>{
+            console.log(7);
+       
+            setTimeout(()=>{
+                console.log(6);
+               
+                setTimeout(()=>{
+                    console.log(5);
+                  
+                    setTimeout(()=>{
+                        console.log(4);
+                        
+                        setTimeout(()=>{
+                            console.log(3);
+                          
+                            setTimeout(()=>{
+                                console.log(2);
+                               
+                                setTimeout(()=>{
+                                    console.log(1);
+                                    
+                                    var setting = setTimeout(()=>{
+                                        console.log("Happy Independence day");  
+                                        document.querySelector('body').append("Happy Independence day")                                     
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+},1000)
